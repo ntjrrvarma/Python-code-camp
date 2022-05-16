96ba38b1b6b4cab8ab00d2f37ce191e98ec89cfa
Rahul N R <31485636+ntjrrvarma@users.noreply.github.com>
2022-05-16 10:49:28 +0000

    Tip Calculator

--- day2.py
+++ day2.py
@@ -8,8 +8,11 @@
 #Write your code below this line 👇
 
 print("Welcome to the tip calculator!")
+
 amount = int(input("What was the total bill? $"))
+
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+
 people = int(input("How many people to split the bill? "))
 
 amt = (amount / people) * (1 + (tip / 100))
